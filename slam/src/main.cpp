8bd1a67c8bb29671426f90c4272a8a4de8d762fa
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-03-26 16:16:01 +0000

    Off file reading

--- slam/src/main.cpp
+++ slam/src/main.cpp
@@ -1,21 +1,45 @@
 #include <bitset>
 #include <iostream>
+#include <fstream>
 
 #include "../include/octomap/Octomap.h"
 
 using namespace std;
-
 using namespace octomap;
+
+// https://segeval.cs.princeton.edu/public/off_format.html
+vector<Vector3f> importOff(const string &filename) {
+    ifstream f(filename);
+
+    f.ignore(9999, '\n'); // first line should say OFF
+    int vertCnt, faceCnt, edgeCnt; // edge cnt is usually ignored
+    f >> vertCnt >> faceCnt >> edgeCnt;
+
+    vector<Vector3f> ret;
+    float x, y, z;
+    for (int i = 0; i < vertCnt; ++i) {
+        f >> x >> y >> z;
+        ret.emplace_back(x, y, z);
+    }
+
+    // we don't care about the object faces
+    f.close();
+    return ret;
+}
 
 int main() {
     Octomap o = Octomap();
 
-    auto ray = o.rayCast(Vector3<float>(0, 0, 0), Vector3<float>(1.0, 1.0, 1.0));
-    for (auto & it : ray) {
-        cout << it.toCoord() << endl;
-        o.setOccupancy(it, 1.0);
+    for (const auto& point : importOff("toilet_0398.off")) {
+        o.setFull(point);
     }
-    o.setFull(Vector3f(1.0, 1.0, 1.0));
+
+    //auto ray = o.rayCast(Vector3<float>(0, 0, 0), Vector3<float>(1.0, 1.0, 1.0));
+    //for (auto & it : ray) {
+    //    cout << it.toCoord() << endl;
+    //    o.setOccupancy(it, 1.0);
+    //}
+    //o.setFull(Vector3f(1.0, 1.0, 1.0));
 
     //int base = 0;
     //for (int x = base + -20; x < base + 20; x++) {
