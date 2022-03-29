130d2c94effdb4ed454231141e83c7c54730a3db
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-03-29 15:22:58 +0100

    Clean-up main function.

--- slam/src/main.cpp
+++ slam/src/main.cpp
@@ -51,45 +51,6 @@ int main() {
     //}
     //o.setFull(Vector3f(1.0, 1.0, 1.0));
 
-    //int base = 0;
-    //for (int x = base + -20; x < base + 20; x++) {
-    //    for (int y = base + -20; y < base + 20; y++) {
-    //        for (int z = base + -20; z < base + 20; z++) {
-    //            Vector3 endpoint((float) x * 0.05f, (float) y * 0.05f, (float) z * 0.05f);
-    //            // integrate 'occupied' measurement
-    //            o.setFull(endpoint);
-    //        }
-    //    }
-    //}
-
-    //base = -20;
-    //for (int x = base + -20; x < base + 20; x++) {
-    //    for (int y = base + -20; y < base + 20; y++) {
-    //        for (int z = base + -20; z < base + 20; z++) {
-    //            Vector3 endpoint((float) x * 0.05f, (float) y * 0.05f, (float) z * 0.05f);
-    //            // integrate 'occupied' measurement
-    //            o.setFull(endpoint);
-    //        }
-    //    }
-    //}
-
-    //base = 20;
-    //for (int x = base + -20; x < base + 20; x++) {
-    //    for (int y = base + -20; y < base + 20; y++) {
-    //        for (int z = base + -20; z < base + 20; z++) {
-    //            Vector3 endpoint((float) x * 0.05f, (float) y * 0.05f, (float) z * 0.05f);
-    //            // integrate 'occupied' measurement
-    //            o.setFull(endpoint);
-    //        }
-    //    }
-    //}
-
-    //OcNode *n;
-    //n = o.search(Vector3<>(1000, 1000, 1000));
-    //cout << ((n != nullptr) ? "findei\n" : "n encontralhei\n");
-    //n = o.search(Vector3<>(0, 0, 0));
-    //cout << ((n != nullptr) ? "findei\n" : "n encontralhei\n");
-
     cout << "Size: " << o.getSize() << endl;
     o.writeBinary("rust.bt");
 
