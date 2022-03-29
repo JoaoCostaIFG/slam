8f99f73c9b8ebd9b43c36554502c030eb6d31d07
João Martins <joaolucassilvamartins@gmail.com>
2022-03-29 21:48:21 +0100

    Add: Import map to c++. Still need to export python to map

--- slam/src/main.cpp
+++ slam/src/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 
 #include "../include/octomap/Octomap.h"
+#include "../include/Scan.h"
 
 using namespace std;
 using namespace octomap;
@@ -28,7 +29,7 @@ vector<Vector3f> importOff(const string &filename) {
 }
 
 int main() {
-    Octomap o = Octomap();
+    //Octomap o = Octomap();
 
     //o.rayCast(Vector3(), Vector3f(1, 1, 1));
     //o.rayCast(Vector3(), Vector3f(1267.09998, 2835.5, 272.221985));
@@ -37,7 +38,7 @@ int main() {
     //}
     //o.setEmpty(Vector3f(67.09998, 35.5, 2.221985));
 
-    o.pointcloudUpdate(vector<Vector3f>({Vector3f(67.09998, 35.5, 2.221985)}), Vector3f());
+    //o.pointcloudUpdate(vector<Vector3f>({Vector3f(67.09998, 35.5, 2.221985)}), Vector3f());
     //o.pointcloudUpdate(importOff("../datasets/airplane_small.off"), Vector3f());
 
     //for (const auto& point : importOff("../datasets/airplane_small.off")) {
@@ -51,8 +52,14 @@ int main() {
     //}
     //o.setFull(Vector3f(1.0, 1.0, 1.0));
 
-    cout << "Size: " << o.getSize() << endl;
-    o.writeBinary("rust.bt");
+    //cout << "Size: " << o.getSize() << endl;
+    //o.writeBinary("rust.bt");
+
+    ifstream ss("deletethis.json");
+    Scan *s = Scan::importJson(ss);
+    cout << "Scan: " << *s << endl;
+    for (auto beam: s->getBeams())
+        cout << *beam << endl;
 
     return EXIT_SUCCESS;
 }
