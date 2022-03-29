7d594771eb416e3a40bce18fa5d7186c11894d06
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-03-29 21:54:19 +0100

    Add code style things

--- slam/include/octomap/OcNodeKey.h
+++ slam/include/octomap/OcNodeKey.h
@@ -32,46 +32,48 @@ namespace octomap {
         virtual void set(unsigned int i, unsigned int val) = 0;
 
         bool operator==(const OcNodeKey &rhs) const {
-            return this->get(0) == rhs.get(0) &&
-                   this->get(1) == rhs.get(1) &&
-                   this->get(2) == rhs.get(2);
+          return this->get(0) == rhs.get(0) &&
+                 this->get(1) == rhs.get(1) &&
+                 this->get(2) == rhs.get(2);
         }
 
         bool operator!=(const OcNodeKey &rhs) const {
-            return !(rhs == *this);
+          return !(rhs == *this);
         }
 
         static void setMaxCoord(unsigned int mc) {
-            OcNodeKey::maxCoord = mc;
+          OcNodeKey::maxCoord = mc;
         }
 
         static void setResolution(double r) {
-            OcNodeKey::resolution = r;
-            OcNodeKey::resolution_factor = 1.0 / r;
+          OcNodeKey::resolution = r;
+          OcNodeKey::resolution_factor = 1.0 / r;
         }
 
         struct Cmp {
             bool operator()(const OcNodeKey &a, const OcNodeKey &b) const {
-                return a == b;
+              return a == b;
             }
 
             bool operator()(const std::unique_ptr<OcNodeKey> &a, const std::unique_ptr<OcNodeKey> &b) const {
-                return this->operator()(*a, *b);
+              return this->operator()(*a, *b);
             }
         };
 
         struct Hash {
             size_t operator()(const OcNodeKey &key) const {
-                return (size_t) key.get(0) +
-                       2287 * (size_t) key.get(1) +
-                       104729 * (size_t) key.get(2);
+              return (size_t) key.get(0) +
+                     2287 * (size_t) key.get(1) +
+                     104729 * (size_t) key.get(2);
             }
 
             size_t operator()(const std::unique_ptr<OcNodeKey> &key) const {
-                return this->operator()(*key);
+              return this->operator()(*key);
             }
         };
     };
+
+    typedef std::unordered_set<std::unique_ptr<OcNodeKey>, OcNodeKey::Hash, OcNodeKey::Cmp> KeySet;
 
     template<typename T = uint16_t>
     class OcNodeKeyInt : public OcNodeKey {
@@ -79,71 +81,71 @@ namespace octomap {
         T k[3];
 
         static T coord2key(float coord) {
-            return (T) floor(resolution_factor * coord) + maxCoord;
+          return (T) floor(resolution_factor * coord) + maxCoord;
         }
 
         [[nodiscard]] static float key2coord(T key) {
-            return float((key - maxCoord + 0.5) * resolution);
+          return float((key - maxCoord + 0.5) * resolution);
         }
 
     public:
         explicit OcNodeKeyInt(const Vector3<> &p) : k{
-                coord2key(p[0]),
-                coord2key(p[1]),
-                coord2key(p[2])
+            coord2key(p[0]),
+            coord2key(p[1]),
+            coord2key(p[2])
         } {}
 
         OcNodeKeyInt() : OcNodeKeyInt(Vector3()) {}
 
         explicit OcNodeKeyInt(const OcNodeKey &other) :
-                k{(T) other.get(0), (T) other.get(1), (T) other.get(2)} {};
+            k{(T) other.get(0), (T) other.get(1), (T) other.get(2)} {};
 
         ~OcNodeKeyInt() = default;
 
         [[nodiscard]] uint8_t getStep(unsigned int i) const override {
-            T mask = (1 << i);
-            return bool((*this)[0] & mask) * 1 +
-                   bool((*this)[1] & mask) * 2 +
-                   bool((*this)[2] & mask) * 4;
+          T mask = (1 << i);
+          return bool((*this)[0] & mask) * 1 +
+                 bool((*this)[1] & mask) * 2 +
+                 bool((*this)[2] & mask) * 4;
         }
 
         [[nodiscard]] float toCoord(unsigned int i) const override {
-            return OcNodeKeyInt::key2coord((*this)[i]);
+          return OcNodeKeyInt::key2coord((*this)[i]);
         }
 
         [[nodiscard]] Vector3<> toCoord() const override {
-            return {
-                    this->toCoord(0),
-                    this->toCoord(1),
-                    this->toCoord(2),
-            };
+          return {
+              this->toCoord(0),
+              this->toCoord(1),
+              this->toCoord(2),
+          };
         }
 
         [[nodiscard]] unsigned long get(unsigned int i) const override {
-            assert(i < 3);
-            return this->k[i];
+          assert(i < 3);
+          return this->k[i];
         }
 
         void set(unsigned int i, unsigned int val) override {
-            assert(i < 3);
-            this->k[i] = val;
+          assert(i < 3);
+          this->k[i] = val;
         };
 
         const T &operator[](unsigned int i) const {
-            assert(i < 3);
-            return k[i];
+          assert(i < 3);
+          return k[i];
         }
 
         T &operator[](unsigned int i) {
-            assert(i < 3);
-            return k[i];
+          assert(i < 3);
+          return k[i];
         }
 
         friend std::ostream &operator<<(std::ostream &out, OcNodeKeyInt const &key) {
-            // TODO should adapt
-            return out << "(" << std::bitset<16>(key[0]) <<
-                       " " << std::bitset<16>(key[1]) <<
-                       " " << std::bitset<16>(key[2]) << ")";
+          // TODO should adapt
+          return out << "(" << std::bitset<16>(key[0]) <<
+                     " " << std::bitset<16>(key[1]) <<
+                     " " << std::bitset<16>(key[2]) << ")";
         }
     };
 
@@ -153,97 +155,97 @@ namespace octomap {
         unsigned int bitCnt = 16;
 
         static unsigned long coord2key(float coord) {
-            return (unsigned long) (floor(resolution_factor * coord) + maxCoord);
+          return (unsigned long) (floor(resolution_factor * coord) + maxCoord);
         }
 
         [[nodiscard]] static float key2coord(const boost::dynamic_bitset<> &key) {
-            return float((key.to_ulong() - maxCoord + 0.5) * resolution);
+          return float((key.to_ulong() - maxCoord + 0.5) * resolution);
         }
 
     public:
         explicit OcNodeKeyBitSet(unsigned int bitCnt, const Vector3<> &p) : k{
-                boost::dynamic_bitset<>(bitCnt, coord2key(p[0])),
-                boost::dynamic_bitset<>(bitCnt, coord2key(p[1])),
-                boost::dynamic_bitset<>(bitCnt, coord2key(p[2])),
+            boost::dynamic_bitset<>(bitCnt, coord2key(p[0])),
+            boost::dynamic_bitset<>(bitCnt, coord2key(p[1])),
+            boost::dynamic_bitset<>(bitCnt, coord2key(p[2])),
         }, bitCnt(bitCnt) {}
 
         explicit OcNodeKeyBitSet(unsigned int bitCnt) : OcNodeKeyBitSet(bitCnt, Vector3()) {}
 
         OcNodeKeyBitSet(unsigned int bitCnt, const OcNodeKey &o) :
-                k{
-                        boost::dynamic_bitset<>(bitCnt, o.get(0)),
-                        boost::dynamic_bitset<>(bitCnt, o.get(1)),
-                        boost::dynamic_bitset<>(bitCnt, o.get(2)),
-                }, bitCnt(bitCnt) {}
+            k{
+                boost::dynamic_bitset<>(bitCnt, o.get(0)),
+                boost::dynamic_bitset<>(bitCnt, o.get(1)),
+                boost::dynamic_bitset<>(bitCnt, o.get(2)),
+            }, bitCnt(bitCnt) {}
 
         ~OcNodeKeyBitSet() = default;
 
         [[nodiscard]] uint8_t getStep(unsigned int i) const override {
-            unsigned long mask = (1 << i);
-            return bool(this->get(0) & mask) * 1 +
-                   bool(this->get(1) & mask) * 2 +
-                   bool(this->get(2) & mask) * 4;
+          unsigned long mask = (1 << i);
+          return bool(this->get(0) & mask) * 1 +
+                 bool(this->get(1) & mask) * 2 +
+                 bool(this->get(2) & mask) * 4;
         }
 
         [[nodiscard]] float toCoord(unsigned int i) const override {
-            return key2coord(this->k[i]);
+          return key2coord(this->k[i]);
         }
 
         [[nodiscard]] Vector3<> toCoord() const override {
-            return {
-                    key2coord(this->k[0]),
-                    key2coord(this->k[1]),
-                    key2coord(this->k[2])
-            };
+          return {
+              key2coord(this->k[0]),
+              key2coord(this->k[1]),
+              key2coord(this->k[2])
+          };
         }
 
         [[nodiscard]] unsigned long get(unsigned int i) const override {
-            return this->k[i].to_ulong();
+          return this->k[i].to_ulong();
         }
 
         void set(unsigned int i, unsigned int val) override {
-            assert(i < 3);
-            this->k[i] = boost::dynamic_bitset(bitCnt, val);
+          assert(i < 3);
+          this->k[i] = boost::dynamic_bitset(bitCnt, val);
         }
 
         unsigned long operator[](unsigned int i) const {
-            assert(i < 3);
-            return this->get(i);
+          assert(i < 3);
+          return this->get(i);
         }
 
         friend std::ostream &operator<<(std::ostream &out, OcNodeKeyBitSet const &key) {
-            return out << "(" << key[0] <<
-                       " " << key[1] <<
-                       " " << key[2] << ")";
+          return out << "(" << key[0] <<
+                     " " << key[1] <<
+                     " " << key[2] << ")";
         }
     };
 
     inline std::unique_ptr<OcNodeKey> newOcNodeKey(unsigned int maxDepth, const Vector3f &initializer = Vector3()) {
-        if (maxDepth <= 8) {
-            return std::make_unique<OcNodeKeyInt<uint8_t>>(initializer);
-        } else if (maxDepth <= 16) {
-            return std::make_unique<OcNodeKeyInt<uint16_t>>(initializer);
-        } else if (maxDepth <= 32) {
-            return std::make_unique<OcNodeKeyInt<uint32_t>>(initializer);
-        } else if (maxDepth <= 64) {
-            return std::make_unique<OcNodeKeyInt<uint64_t>>(initializer);
-        } else {
-            return std::make_unique<OcNodeKeyBitSet>(maxDepth, initializer);
-        }
+      if (maxDepth <= 8) {
+        return std::make_unique<OcNodeKeyInt<uint8_t>>(initializer);
+      } else if (maxDepth <= 16) {
+        return std::make_unique<OcNodeKeyInt<uint16_t>>(initializer);
+      } else if (maxDepth <= 32) {
+        return std::make_unique<OcNodeKeyInt<uint32_t>>(initializer);
+      } else if (maxDepth <= 64) {
+        return std::make_unique<OcNodeKeyInt<uint64_t>>(initializer);
+      } else {
+        return std::make_unique<OcNodeKeyBitSet>(maxDepth, initializer);
+      }
     }
 
     inline std::unique_ptr<OcNodeKey> newOcNodeKey(unsigned int maxDepth, const OcNodeKey &other) {
-        if (maxDepth <= 8) {
-            return std::make_unique<OcNodeKeyInt<uint8_t>>(other);
-        } else if (maxDepth <= 16) {
-            return std::make_unique<OcNodeKeyInt<uint16_t>>(other);
-        } else if (maxDepth <= 32) {
-            return std::make_unique<OcNodeKeyInt<uint32_t>>(other);
-        } else if (maxDepth <= 64) {
-            return std::make_unique<OcNodeKeyInt<uint64_t>>(other);
-        } else {
-            return std::make_unique<OcNodeKeyBitSet>(maxDepth, other);
-        }
+      if (maxDepth <= 8) {
+        return std::make_unique<OcNodeKeyInt<uint8_t>>(other);
+      } else if (maxDepth <= 16) {
+        return std::make_unique<OcNodeKeyInt<uint16_t>>(other);
+      } else if (maxDepth <= 32) {
+        return std::make_unique<OcNodeKeyInt<uint32_t>>(other);
+      } else if (maxDepth <= 64) {
+        return std::make_unique<OcNodeKeyInt<uint64_t>>(other);
+      } else {
+        return std::make_unique<OcNodeKeyBitSet>(maxDepth, other);
+      }
     }
 }
 
--- slam/src/Octomap.cpp
+++ slam/src/Octomap.cpp
@@ -2,8 +2,11 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+
 #ifdef _OPENMP
+
 #include <omp.h>
+
 #endif
 
 #include "../include/octomap/Octomap.h"
@@ -11,193 +14,193 @@
 using namespace octomap;
 
 Octomap::Octomap(const unsigned int maxDepth, const double resolution) :
-        depth(maxDepth), resolution(resolution) {
-    assert(this->depth >= 1);
-    OcNodeKey::setMaxCoord((int) pow(2, maxDepth - 1));
-    OcNodeKey::setResolution(resolution);
+    depth(maxDepth), resolution(resolution) {
+  assert(this->depth >= 1);
+  OcNodeKey::setMaxCoord((int) pow(2, maxDepth - 1));
+  OcNodeKey::setResolution(resolution);
 
-    this->rootNode = nullptr;
+  this->rootNode = nullptr;
 
-    // pre-calculate step sizes
-    this->stepLookupTable.reserve(this->depth + 2);
-    for (unsigned int i = 0; i <= this->depth; ++i) {
-        // equivalent to: 2^depth * resolution
-        this->stepLookupTable[i] = this->resolution * double(1 << (this->depth - i));
-    }
-    this->stepLookupTable[this->depth + 1] = this->resolution / 2.0;
-    // tree center for calculations
-    //this->treeCenter = Vector3((float) (this->stepLookupTable[0] / 2.0));
+  // pre-calculate step sizes
+  this->stepLookupTable.reserve(this->depth + 2);
+  for (unsigned int i = 0; i <= this->depth; ++i) {
+    // equivalent to: 2^depth * resolution
+    this->stepLookupTable[i] = this->resolution * double(1 << (this->depth - i));
+  }
+  this->stepLookupTable[this->depth + 1] = this->resolution / 2.0;
+  // tree center for calculations
+  //this->treeCenter = Vector3((float) (this->stepLookupTable[0] / 2.0));
 }
 
 Octomap::Octomap() : Octomap(DFLT_MAX_DEPTH, DFLT_RESOLUTION) {}
 
 
 Octomap::~Octomap() {
-    delete this->rootNode;
+  delete this->rootNode;
 }
 
 bool Octomap::createRootIfNeeded() {
-    if (this->rootNode == nullptr) {
-        this->rootNode = new OcNode();
-        return true;
-    }
-    return false;
+  if (this->rootNode == nullptr) {
+    this->rootNode = new OcNode();
+    return true;
+  }
+  return false;
 }
 
 OcNode *Octomap::setOccupancy(const OcNodeKey &key, float occ) {
-    bool createdRoot = this->createRootIfNeeded();
-    return this->rootNode->setOccupancy(key, this->depth, occ, createdRoot);
+  bool createdRoot = this->createRootIfNeeded();
+  return this->rootNode->setOccupancy(key, this->depth, occ, createdRoot);
 }
 
 OcNode *Octomap::setOccupancy(const Vector3<> &location, const float occ) {
-    return this->setOccupancy(*newOcNodeKey(this->depth, location), occ);
+  return this->setOccupancy(*newOcNodeKey(this->depth, location), occ);
 }
 
 OcNode *Octomap::updateLogOdds(const OcNodeKey &key, const float logOdds) {
-    // We do a search before updating the target node. This small overhead can save a lot
-    // of time in the long-run. Note: the search takes O(l) time, where l is the max depth, which is constant.
-    // If the node already exists, we can see if the update would change its log-odds. If the node is stable
-    // (+/- 0 affected) or the log-odds is 0, the update wouldn't change anything, but we would still need to
-    // perform the intermediate node updates: the intermediate nodes wouldn't change, but the check would be performed.
-    auto s = this->search(key);
-    if (s && !s->wouldChange(logOdds)) return s;
+  // We do a search before updating the target node. This small overhead can save a lot
+  // of time in the long-run. Note: the search takes O(l) time, where l is the max depth, which is constant.
+  // If the node already exists, we can see if the update would change its log-odds. If the node is stable
+  // (+/- 0 affected) or the log-odds is 0, the update wouldn't change anything, but we would still need to
+  // perform the intermediate node updates: the intermediate nodes wouldn't change, but the check would be performed.
+  auto s = this->search(key);
+  if (s && !s->wouldChange(logOdds)) return s;
 
-    bool createdRoot = this->createRootIfNeeded();
-    return this->rootNode->updateLogOdds(key, this->depth, logOdds, createdRoot);
+  bool createdRoot = this->createRootIfNeeded();
+  return this->rootNode->updateLogOdds(key, this->depth, logOdds, createdRoot);
 }
 
 OcNode *Octomap::updateOccupancy(const OcNodeKey &key, const float occ) {
-    return this->updateLogOdds(key, (float) OcNode::prob2logodds(occ));
+  return this->updateLogOdds(key, (float) OcNode::prob2logodds(occ));
 }
 
 OcNode *Octomap::updateOccupancy(const Vector3<> &location, const float occ) {
-    return this->updateOccupancy(*newOcNodeKey(this->depth, location), occ);
+  return this->updateOccupancy(*newOcNodeKey(this->depth, location), occ);
 }
 
 OcNode *Octomap::search(const OcNodeKey &key) {
-    if (this->rootNode == nullptr) return nullptr;
-    return this->rootNode->search(key, this->depth);
+  if (this->rootNode == nullptr) return nullptr;
+  return this->rootNode->search(key, this->depth);
 }
 
 OcNode *Octomap::search(const Vector3<> &location) {
-    if (this->rootNode == nullptr) return nullptr;
-    return this->search(*newOcNodeKey(this->depth, location));
+  if (this->rootNode == nullptr) return nullptr;
+  return this->search(*newOcNodeKey(this->depth, location));
 }
 
 std::vector<std::unique_ptr<OcNodeKey>> Octomap::rayCast(const Vector3<> &orig, const Vector3<> &end) {
-    auto ray = std::vector<std::unique_ptr<OcNodeKey>>();
+  auto ray = std::vector<std::unique_ptr<OcNodeKey>>();
 
-    auto coord = newOcNodeKey(this->depth, orig);
-    auto endKey = newOcNodeKey(this->depth, end);
-    if (coord == endKey) return ray;
+  auto coord = newOcNodeKey(this->depth, orig);
+  auto endKey = newOcNodeKey(this->depth, end);
+  if (coord == endKey) return ray;
 
-    // Initialization phase
-    auto step = Vector3i();
-    auto tMax = Vector3d();
-    auto tDelta = Vector3d();
+  // Initialization phase
+  auto step = Vector3i();
+  auto tMax = Vector3d();
+  auto tDelta = Vector3d();
 
-    auto direction = (end - orig);
-    direction.normalize();
+  auto direction = (end - orig);
+  direction.normalize();
 
-    for (int i = 0; i < 3; ++i) {
-        if (direction[i] > 0) step[i] = 1.0;
-        else step[i] = -1.0;
+  for (int i = 0; i < 3; ++i) {
+    if (direction[i] > 0) step[i] = 1.0;
+    else step[i] = -1.0;
 
-        double voxelBorder = coord->toCoord(i) +
-                             step[i] * this->stepLookupTable[this->depth + 1];
+    double voxelBorder = coord->toCoord(i) +
+                         step[i] * this->stepLookupTable[this->depth + 1];
 
-        tMax[i] = (voxelBorder - orig[i]) / direction[i];
-        tDelta[i] = this->resolution / fabs(direction[i]);
-    }
+    tMax[i] = (voxelBorder - orig[i]) / direction[i];
+    tDelta[i] = this->resolution / fabs(direction[i]);
+  }
 
-    // Incremental phase
-    Vector3 origCord = coord->toCoord();
-    double length = (endKey->toCoord() - origCord).norm();
-    double *min;
-    while (*coord != *endKey &&
-           (
-                   *(min = std::min_element(tMax.begin(), tMax.end())) <= length ||
-                   (coord->toCoord() - origCord).norm() <= length
-           )) {
-        //std::cout << coord->get(0) << " " << coord->get(1) << " " << coord->get(2) <<
-        //          " " << coord->toCoord()
-        //          << std::endl;
-        auto newCoord = newOcNodeKey(this->depth, *coord);
-        ray.push_back(std::move(coord));
-        int coordInd = int(min - std::begin(tMax));
+  // Incremental phase
+  Vector3 origCord = coord->toCoord();
+  double length = (endKey->toCoord() - origCord).norm();
+  double *min;
+  while (*coord != *endKey &&
+         (
+             *(min = std::min_element(tMax.begin(), tMax.end())) <= length ||
+             (coord->toCoord() - origCord).norm() <= length
+         )) {
+    //std::cout << coord->get(0) << " " << coord->get(1) << " " << coord->get(2) <<
+    //          " " << coord->toCoord()
+    //          << std::endl;
+    auto newCoord = newOcNodeKey(this->depth, *coord);
+    ray.push_back(std::move(coord));
+    int coordInd = int(min - std::begin(tMax));
 
-        tMax[coordInd] += tDelta[coordInd];
-        coord = std::move(newCoord);
-        coord->set(coordInd, coord->get(coordInd) + step[coordInd]);
-    }
+    tMax[coordInd] += tDelta[coordInd];
+    coord = std::move(newCoord);
+    coord->set(coordInd, coord->get(coordInd) + step[coordInd]);
+  }
 
-    return ray;
+  return ray;
 }
 
 OcNode *Octomap::rayCastUpdate(const Vector3<> &orig, const Vector3<> &end, float occ) {
-    auto ray = this->rayCast(orig, end);
-    for (auto &it: ray)
-        this->setEmpty(*it);
-    return this->updateOccupancy(end, occ);
+  auto ray = this->rayCast(orig, end);
+  for (auto &it: ray)
+    this->setEmpty(*it);
+  return this->updateOccupancy(end, occ);
 }
 
 void Octomap::pointcloudUpdate(const std::vector<Vector3f> &pointcloud, const Vector3f &origin) {
-    std::unordered_set<std::unique_ptr<OcNodeKey>, OcNodeKey::Hash, OcNodeKey::Cmp> freeNodes, occupiedNodes;
+  KeySet freeNodes, occupiedNodes;
 
 #ifdef _OPENMP
 #pragma omp parallel for schedule(guided) default(none) shared(pointcloud, origin, freeNodes, occupiedNodes)
 #endif
-    for (const auto &endpoint: pointcloud) {
-        auto ray = this->rayCast(origin, endpoint);
+  for (const auto &endpoint: pointcloud) {
+    auto ray = this->rayCast(origin, endpoint);
 #ifdef _OPENMP
 #pragma omp critical (freeNodes_insert)
 #endif
-        {
-            for (auto &rayPoint: ray) {
-                freeNodes.insert(std::move(rayPoint));
-            }
-        }
+    {
+      for (auto &rayPoint: ray) {
+        freeNodes.insert(std::move(rayPoint));
+      }
+    }
 #ifdef _OPENMP
 #pragma omp critical (occupiedNodes_insert)
 #endif
-        {
-            occupiedNodes.insert(newOcNodeKey(this->depth, endpoint));
-        }
+    {
+      occupiedNodes.insert(newOcNodeKey(this->depth, endpoint));
     }
+  }
 
-    // remove updates on freenodes that will be set as occupied
-    for (auto it = freeNodes.begin(); it != freeNodes.end();) {
-        if (occupiedNodes.find(*it) != occupiedNodes.end()) {
-            it = freeNodes.erase(it);
-        } else {
-            ++it;
-        }
+  // remove updates on freenodes that will be set as occupied
+  for (auto it = freeNodes.begin(); it != freeNodes.end();) {
+    if (occupiedNodes.find(*it) != occupiedNodes.end()) {
+      it = freeNodes.erase(it);
+    } else {
+      ++it;
     }
+  }
 
-    // update nodes
-    for (auto &endpoint: freeNodes) {
-        this->updateOccupancy(*endpoint, 0);
-    }
-    for (auto &endpoint: occupiedNodes) {
-        this->updateOccupancy(*endpoint, 1);
-    }
+  // update nodes
+  for (auto &endpoint: freeNodes) {
+    this->updateOccupancy(*endpoint, 0);
+  }
+  for (auto &endpoint: occupiedNodes) {
+    this->updateOccupancy(*endpoint, 1);
+  }
 }
 
 bool Octomap::writeBinary(std::ostream &os) {
-    os << "# Octomap OcTree binary file\n";
-    os << "id OcTree\n";
-    os << "size " << this->getSize() << std::endl;
-    os << "res " << this->resolution << std::endl;
-    os << "data" << std::endl;
-    if (this->rootNode == nullptr) return false;
-    this->rootNode->writeBinary(os);
-    return true;
+  os << "# Octomap OcTree binary file\n";
+  os << "id OcTree\n";
+  os << "size " << this->getSize() << std::endl;
+  os << "res " << this->resolution << std::endl;
+  os << "data" << std::endl;
+  if (this->rootNode == nullptr) return false;
+  this->rootNode->writeBinary(os);
+  return true;
 }
 
 bool Octomap::writeBinary(const std::string &filename) {
-    std::ofstream binary_outfile(filename.c_str(), std::ios_base::binary);
-    if (!binary_outfile.is_open()) {
-        return false;
-    }
-    return writeBinary(binary_outfile);
+  std::ofstream binary_outfile(filename.c_str(), std::ios_base::binary);
+  if (!binary_outfile.is_open()) {
+    return false;
+  }
+  return writeBinary(binary_outfile);
 }
