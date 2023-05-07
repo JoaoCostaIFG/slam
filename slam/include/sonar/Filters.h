a17224106f7cb9863e8b6ad4d82dca5ecf12d836
Tiago Silva <tiagodusilva@gmail.com>
2023-05-07 17:48:48 +0000

    Refactor: Improved readibility of displaySweep

--- slam/include/sonar/Filters.h
+++ slam/include/sonar/Filters.h
@@ -44,51 +44,61 @@ namespace sonar {
 
   /**
    *
+   * Generates an image representation of a sweep under the cartesian system, for a given sweep.
+   * Internal for displaySweep()
+   * @param sweep the sweep from which to generate the image
+   * @param img the image into which the image will be generated
+   */
+  static void generateSweepCartesian(const Sweep& sweep, cv::Mat& img) {
+    size_t n = sweep.getBeamLen() * 2;
+    std::vector<int> vote_intensities(n * n, 0);
+    std::vector<int> vote_count(n * n, 0);
+    img = cv::Mat::zeros(n, n, CV_8U);
+
+    // Gather votes
+    int x0 = sweep.getBeamLen(), y0 = sweep.getBeamLen();
+    for (const Beam* beam: sweep.getBeams()) {
+      double base_angle = beam->getAngle();
+      for (double angle = base_angle - 1.5;
+           angle < base_angle + 1.601; angle += 0.1) { // TODO Replace 1.601 with angle step
+        // 180 => pi
+        // deg => rad
+        for (size_t i = 0; i < sweep.getBeamLen(); ++i) {
+          const uint8_t& intensity = beam->at(i);
+          double angle_rad = ((angle + 180) * CV_PI) / 180;
+          Vector3<> v = beam->measurementToCartesian(i, angle_rad);
+          int x = x0 + round(v.x()), y = y0 + round(v.y());
+          int pos = y + x * n;
+          vote_intensities[pos] += intensity;
+          vote_count[pos] += 1;
+        }
+      }
+    }
+
+    for (size_t y = 0; y < n; y++) {
+      for (size_t x = 0; x < n; x++) {
+        int pos = y + x * n;
+        int cnt = vote_count[pos];
+        if (cnt != 0) {
+          uint8_t mean = round(vote_intensities[pos] / (double) cnt);
+          img.at<uint8_t>(y, x) = mean;
+        }
+      }
+    }
+  }
+
+  /**
+   *
    * Opens an openCV window with a representation, which may be cartesian or polar, of the given sweep. After the image
    * is shown, use q to close the window.
    * @param sweep the sweep that will be displayed
    * @param inCartesian if true, the the data will be displayed in cartesian coordinates. Otherwise, the polar
    * representation will be used.
    */
-  void displaySweep(const Sweep& sweep, const bool& inCartesian = false) {
+  void displaySweep(const Sweep& sweep, const bool inCartesian = false) {
     cv::Mat img;
     if (inCartesian) {
-      size_t n = sweep.getBeamLen() * 2;
-      std::vector<int> vote_intensities(n * n, 0);
-      std::vector<int> vote_count(n * n, 0);
-      img = cv::Mat::zeros(n, n, CV_8U);
-
-      // Gather votes
-      int x0 = sweep.getBeamLen(), y0 = sweep.getBeamLen();
-      for (const Beam* beam: sweep.getBeams()) {
-        double base_angle = beam->getAngle();
-        for (double angle = base_angle - 1.5;
-             angle < base_angle + 1.601; angle += 0.1) { // TODO Replace 1.601 with angle step
-          // 180 => pi
-          // deg => rad
-          for (size_t i = 0; i < sweep.getBeamLen(); ++i) {
-            const uint8_t& intensity = beam->at(i);
-            double angle_rad = ((angle + 180) * CV_PI) / 180;
-            Vector3<> v = beam->measurementToCartesian(i, angle_rad);
-            int x = x0 + round(v.x()), y = y0 + round(v.y());
-            int pos = y + x * n;
-            vote_intensities[pos] += intensity;
-            vote_count[pos] += 1;
-          }
-        }
-      }
-
-      for (size_t y = 0; y < n; y++) {
-        for (size_t x = 0; x < n; x++) {
-          int pos = y + x * n;
-          int cnt = vote_count[pos];
-          if (cnt != 0) {
-            uint8_t mean = round(vote_intensities[pos] / (double) cnt);
-            img.at<uint8_t>(y, x) = mean;
-          }
-        }
-      }
-
+      generateSweepCartesian(sweep, img);
     } else { // Polar Coordinates
       // img = cv::Mat(sweep.getBeams().size(), sweep.getBeamLen(), CV_8U, sweep.getIntensities().data());
       img = sweep.getIntensities();
